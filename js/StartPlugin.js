0ac942fce7eac2cfdf564b7be98bfe8a4a51b110
Barakial <Barakiall@gmail.com>
2017-09-18 18:33:16 +0300

    18.09

--- js/StartPlugin.js
+++ js/StartPlugin.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
     $('.bxslider').bxSlider({
         mode: 'vertical',
         slideWidth: 300,
-        minSlides: 4,
-        maxSlides: 4,
+        maxSlides: 5,
         slideMargin: 10,
         nextText: 'qwe',
         prevText: 'qwe',
@@ -24,5 +23,40 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         }
     });
+
+    $('.photo-video-tabs').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+
+    $(".carousel-product-cart").owlCarousel({
+        loop: false,
+        margin: 40,
+        items: 4,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left shares-prev'></i>", "<i class='fa fa-angle-right shares-next'></i>"],
+        responsive: {
+        0: {
+            items: 1,
+                nav: true
+        },
+        600: {
+            items: 3,
+                nav: false
+        },
+        1000: {
+            items: 4,
+                nav: true,
+                loop: false,
+        }
+    }
+});
     $('.loupe_img').loupe();
 });
--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
-/*Меню выбора языка*/
-$('.active-menu-language').on('click', function () {
-    $('.list-language').toggle();
+$(document).ready(function() {
+
+    /*Меню выбора языка*/
+    $('.active-menu-language').on('click', function () {
+        $('.list-language').toggle();
+    });
+    /* ================================ */
+
+    /*Меню выбора страны*/
+    $('.select-coutri button').on('click', function () {
+        $('.list-coutri').toggle();
+    });
+    /* ================================ */
+
+    /* Меняем картинку при наведение в галерее */
+    $(".slider-init li a img").hover(function(){
+        $('.swap-image').attr('src',$(this).attr('src').replace('thumb/', ''));
+        $('.loupe img').attr('src',$(this).attr('src').replace('thumb/', ''));
+    });
+
+    var imgSwap = [];
+    $(".thumbnails li a img").each(function(){
+        imgUrl = this.src.replace('thumb/', '');
+        imgSwap.push(imgUrl);
+    });
+    /* ================================ */
 });
-/*Меню выбора страны*/
-$('.select-coutri button').on('click', function () {
-    $('.list-coutri').toggle();
-});
+/*
+$(imgSwap).preload();
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+};*/
 
 var map;
 var myLatLng = {lat: 48.465959, lng: 35.04817};
